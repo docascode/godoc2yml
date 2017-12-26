6ef76ec457bea5168c4db0ed44ffcc035a5f8cc5
Zhiliang Xu <zhiliaxu@corp.microsoft.com>
2017-12-26 10:27:02 +0800

    Add params check

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+    "fmt"
     "os"
     "./goyaml"
 )
 
 func main() {
-	// parameters
-	// arg1 directory which contains the package,
-	packageSource := os.Args[1]
-	// arg2 package name
-	packageName := os.Args[2]
-	// arg3 output directory, wher package.yml saved to
-	ymlOutput := os.Args[3]
-	goyaml.GoYAMLGeneration(packageSource, packageName, ymlOutput)
+    if len(os.Args) != 4 {
+        fmt.Println("Usage: ./godoc2yml <package_source_path> <package_name> <yaml_output_path>")
+        os.Exit(-1)
+    }
+
+    // arg1 directory which contains the package,
+    packageSource := os.Args[1]
+    // arg2 package name
+    packageName := os.Args[2]
+    // arg3 output directory, wher package.yml saved to
+    ymlOutput := os.Args[3]
+    goyaml.GoYAMLGeneration(packageSource, packageName, ymlOutput)
 }
 
