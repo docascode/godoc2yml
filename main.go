abc6f48cd4f81075c28c53b354ecb171d7a30c8e
Yannan Wang <yannw@microsoft.com>
2018-01-11 10:51:33 +0800

    Split type from package, add uid and repo

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-    "os"
-    "./goyaml"
+	"os"
+
+	"./goyaml"
 )
 
 func main() {
 	// parameters
-	// arg1 directory which contains the package,
+	// arg1 source package path
 	packageSource := os.Args[1]
-	// arg2 package name
-	packageName := os.Args[2]
-	// arg3 output directory, wher package.yml saved to
-	ymlOutput := os.Args[3]
-	goyaml.GoYAMLGeneration(packageSource, packageName, ymlOutput)
+	// arg2 output directory
+	ymlOutput := os.Args[2]
+	// arg3 package prefix, parent folder strings of package folder, for example: arm is the package prefix of package arm.advisor
+	packagePrefix := os.Args[3]
+	// arg4 source git repo
+	sourceRepo := os.Args[4]
+	goyaml.GoYAMLGeneration(packageSource, ymlOutput, packagePrefix, sourceRepo)
 }
-
